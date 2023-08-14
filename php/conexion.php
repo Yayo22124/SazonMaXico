3d29adc6d651cf06470b4bb75f922db175df590a
Haziel Ortiz <ya-yo22124@outlook.com>
2023-08-14 15:09:16 -0600

    Modificando las ultimas paginas

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,8 @@
 <?php 
-    $server = 'localhost';
-    $DB = 'bdsazonmaxico';
-    $user = "root";
-    $password = '';
+    $server = '154.56.47.52';
+    $DB = 'u117932138_bdsazonmaxico';
+    $user = "u117932138_raul";
+    $password = 'Raul53rrb53';
     try {
         $conexion = new PDO ("mysql:host=$server;dbname=$DB",$user,$password);
     } catch (Exception $e) {
