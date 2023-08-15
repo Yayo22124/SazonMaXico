e8bbbc694a564c7b0d0f717823cbd629d1cb4db5
Haziel Ortiz <ya-yo22124@outlook.com>
2023-08-15 02:42:36 -0600

    Modificando register

--- conexion.php
+++ conexion.php
@@ -1,11 +1,12 @@
-<?php 
-    $server = '154.56.47.52';
-    $DB = 'u117932138_bdsazonmaxico';
-    $user = "u117932138_raul";
-    $password = 'Raul53rrb53';
-    try {
-        $conexion = new PDO ("mysql:host=$server;dbname=$DB",$user,$password);
-    } catch (Exception $e) {
-        echo $e->getMessage();
-    }
+<?php
+$servername = "154.56.47.52";
+$username = "u117932138_raul";
+$password = "Raul53rrb53";
+$database = "u117932138_bdsazonmaxico";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Conexión fallida: " . mysqli_connect_error());
+}
 ?>
